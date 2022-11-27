16db674993c5de3f55c3c484dd944ffa07b62440
w <pink_oops@163.com>
2022-11-27 12:06:06 +0800

    feat: replace println! to eprintln!

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let mini_grep_config = minigrep::MiniGrepConfig::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argusments: {}", err);
+        eprintln!("Problem parsing argusments: {}", err);
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(mini_grep_config) {
-        println!("mini grep running fail:{:?}", err);
+        eprintln!("mini grep running fail:{:?}", err);
         process::exit(1);
     };
 }
